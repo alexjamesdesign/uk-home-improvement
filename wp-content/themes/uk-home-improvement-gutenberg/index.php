75137c7a177e3d0b70022c42d5fa44ca2000ce7a
Alex James <alexjames@Alexs-MacBook-Pro.local>
2022-05-08 11:33:10 +0100

    Frontpage now has rest API blog feed on it. Index has non-rest api cards for each term

--- wp-content/themes/uk-home-improvement-gutenberg/index.php
+++ wp-content/themes/uk-home-improvement-gutenberg/index.php
@@ -4,8 +4,13 @@
     $args = array(
         'taxonomy' => 'category',
         'hide_empty' => false,
+        'parent' => 0
     );
 
     $context['categories'] = Timber::get_terms($args);
+
+    $context['posts'] = new Timber\PostQuery();
+    $context['tax'] = new Timber\Term();
+
 
     Timber::render( [ 'index.twig' ], $context );
